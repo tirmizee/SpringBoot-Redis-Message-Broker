01602550fc2b3027f4b6a2c89a7ea0b64482fcc7
Pratya Yeekhaday <pratya.y@gdl-technology.com>
2020-11-09 15:04:24 +0700

    update properties file.

--- src/main/java/com/tirmizee/SpringBootRedisMessageBrokerApplication.java
+++ src/main/java/com/tirmizee/SpringBootRedisMessageBrokerApplication.java
@@ -1,13 +1,27 @@
 package com.tirmizee;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+import com.tirmizee.service.RedisMessagePublisher;
 
 @SpringBootApplication
-public class SpringBootRedisMessageBrokerApplication {
+public class SpringBootRedisMessageBrokerApplication implements CommandLineRunner {
 
+	@Autowired
+	ApplicationContext applicationContext;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootRedisMessageBrokerApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		RedisMessagePublisher publisher = applicationContext.getBean(RedisMessagePublisher.class);
+		publisher.publishMessage("Hello world.");
 	}
 
 }
