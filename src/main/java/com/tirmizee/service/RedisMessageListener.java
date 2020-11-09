8000be422e0fcbab2d51bfeca09a099c0e2bde33
Pratya Yeekhaday <pratya.y@gdl-technology.com>
2020-11-09 15:01:58 +0700

    add listener

---
+++ src/main/java/com/tirmizee/service/RedisMessageListener.java
@@ -0,0 +1,7 @@
+package com.tirmizee.service;
+
+import org.springframework.data.redis.connection.MessageListener;
+
+public interface RedisMessageListener extends MessageListener {
+
+}
---
+++ src/main/java/com/tirmizee/service/RedisMessageListenerImpl.java
@@ -0,0 +1,14 @@
+package com.tirmizee.service;
+
+import org.springframework.data.redis.connection.Message;
+import org.springframework.stereotype.Service;
+
+@Service
+public class RedisMessageListenerImpl implements RedisMessageListener {
+
+	@Override
+	public void onMessage(Message message, byte[] pattern) {
+		System.out.println("Message received: " + message.toString());		
+	}
+
+}
